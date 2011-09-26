9676966cc11c02a23d9ebcb84f96b6ec21b21680
Finn Jordal <finn@jordal.dk>
2011-09-26 12:54:00 +0200

    forward

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var http = require('http'),
 var options = {
   hostnameOnly: true,
   router: {
-    'nh.oiorest.dk': '127.0.0.1:3000',
+    'nh.oiorest.dk': 'localhost:3000',
     'domaintwo.net': '127.0.0.1:9001',
     'domainthree.org': '127.0.0.1:9002'
   }
+   forward: {
+     host: 'localhost',
+     port: 3000
+   }
 }
 
 var port= 9000;
