33a8fc554ccdd543684a7b1a9c63d6133eb4a174
Finn Jordal <finn@jordal.dk>
2011-10-17 13:05:54 +0200

    rename

---
+++ httpproxyapp.js
@@ -0,0 +1,19 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+var options = {
+  hostnameOnly: true,
+  router: {
+    'nh.oiorest.dk': '127.0.0.1:3000',
+    'domaintwo.net': '127.0.0.1:9001',
+    'domainthree.org': '127.0.0.1:9002'
+  },
+   forward: {
+     host: '127.0.0.1',
+     port: 3000
+   }
+}
+
+var port= 9000;
+var proxyServer = httpProxy.createServer(options).listen(port);
+console.log("Proxy server listening on port %d", port);
