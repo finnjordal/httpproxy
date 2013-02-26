082d5f6953bd035decbe92be60f382cc9f374706
Finn Jordal <finn@jordal.dk>
2013-02-26 13:37:26 +0100

    minus https

--- httpproxyapp.js
+++ httpproxyapp.js
@@ -1,17 +1,17 @@
 var http = require('http')
-  , https= require('https')
+  //, https= require('https')
   , fs= require('fs')
   , httpProxy = require('http-proxy');
 
 var httpOptions = {
   hostnameOnly: true,
   router: {
-	  'postnumre.oiorest.dk': '127.0.0.1:3000',
-    'postnumre.dk': '127.0.0.1:3000'
+	  'vejret.jordal.dk': '127.0.0.1:3000',
+    'jordal.dk': '127.0.0.1:3000'
   }
 }
 
-var httpsOptions = {
+/* var httpsOptions = {
   hostnameOnly: true,
   router: {
     'nh.oiorest.dk': '127.0.0.1:2000',
@@ -21,12 +21,12 @@ var httpsOptions = {
     key: fs.readFileSync('../certificate/privateKey.pem', 'utf8'),
     cert: fs.readFileSync('../certificate/publicCert.pem', 'utf8')
   }
-};
+}; */
 
 var httpPort= 9000;
 var proxyServer = httpProxy.createServer(httpOptions).listen(httpPort);
 console.log(" Http proxy server listening on port %d", httpPort);
 
-var httpsPort= 8000;
+/*var httpsPort= 8000;
 var proxyServer = httpProxy.createServer(httpsOptions).listen(httpsPort);
-console.log(" Https proxy server listening on port %d", httpsPort);
+console.log(" Https proxy server listening on port %d", httpsPort); */
